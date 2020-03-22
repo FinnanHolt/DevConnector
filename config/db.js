ad04ba9b966fc242050c3859020d90bdf4258902
finnanholt <finnanholt@gmail.com>
2020-03-22 19:15:44 +0000

    Added ability to add user to db. Fixed dependencies

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const config = require('config');
 const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
