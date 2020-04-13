aec1807c88d141d1d9348a1ebb50d6e04ae9a496
finnanholt <finnanholt@gmail.com>
2020-04-13 16:14:12 +0100

    Added post and remove comment routes. Simplified unlike a post route. Reformatting.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,12 +12,7 @@ const User = require('../../models/User');
 // @access Private
 router.post(
   '/',
-  [
-    auth,
-    check('text', 'Text is required')
-      .not()
-      .isEmpty()
-  ],
+  [auth, check('text', 'Text is required').not().isEmpty()],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -25,18 +20,16 @@ router.post(
     }
 
     try {
-      let user = await User.findById(req.user.id).select('-password');
-
-      console.log(user);
+      const user = await User.findById(req.user.id).select('-password');
 
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id
+        user: req.user.id,
       });
 
-      let post = await newPost.save();
+      await newPost.save();
 
       res.json(post);
     } catch (error) {
@@ -51,7 +44,7 @@ router.post(
 // @access Private
 router.get('/', auth, async (req, res) => {
   try {
-    let posts = await Post.find().sort({ date: -1 });
+    const posts = await Post.find().sort({ date: -1 });
     return res.json(posts);
   } catch (error) {
     console.error(error.message);
@@ -64,7 +57,7 @@ router.get('/', auth, async (req, res) => {
 // @access Private
 router.get('/:id', auth, async (req, res) => {
   try {
-    let post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id);
     if (!post) {
       return res.status(404).json({ msg: 'Post not found' });
     }
@@ -83,7 +76,7 @@ router.get('/:id', auth, async (req, res) => {
 // @access Private
 router.delete('/:id', auth, async (req, res) => {
   try {
-    let post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id);
 
     if (!post) {
       return res.status(404).json({ msg: 'Post not found' });
@@ -132,27 +125,90 @@ router.put('/like/:id', auth, async (req, res) => {
 // @access Private
 router.put('/unlike/:id', auth, async (req, res) => {
   try {
-    await Post.findById(req.params.id, async (error, result) => {
-      if (!error) {
-        const index = result.likes
-          .map(like => {
-            return like.user;
-          })
-          .indexOf(req.user.id);
+    const post = await Post.findById(req.params.id);
 
-        if (index > -1) {
-          result.likes.splice(index, 1);
-          await result.save();
-          return res.json('Post unliked');
-        } else {
-          return res.status(400).send('You have not liked this post');
-        }
-      }
-      return res.json(error);
+    const like = post.likes.find(like => {
+      return like.user.toString() === req.user.id;
     });
+    if (!like) {
+      return res.status(404).json({ msg: 'Like not found' });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.id.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+    return res.json('Post unliked');
   } catch (error) {
     console.error(error.message);
     return res.status(500).send('Server Error');
+  }
+});
+
+// @route  POST api/posts/comment/:id
+// @desc   Comment on a post
+// @access Private
+router.post(
+  '/comment/:id',
+  [auth, check('text', 'Text is required').not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Delete comment
+// @access Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    if (comment.user.toString() != req.user.id) {
+      return res.status(401).json({ msg: 'User not authorised' });
+    }
+    const removeIndex = post.comments
+      .map(comment => comment.id.toString())
+      .indexOf(req.params.comment_id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
   }
 });
 
