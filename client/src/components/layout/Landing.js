a53034fff798ef912329a4ffe9707e0e7a323137
finnanholt <finnanholt@gmail.com>
2020-05-05 11:56:05 +0100

    Redux store setup

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,7 +5,7 @@ export const Landing = () => {
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
-          <h1 className='x-large'>SocialDev</h1>
+          <h1 className='x-large'>DevConnector</h1>
           <p className='lead'>
             Create a developer profile/portfolio, share posts and get help from
             other developers
